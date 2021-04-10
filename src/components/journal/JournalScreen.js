d94b79ef006f449a3c96e12065cd9ffaaf848f2b
jccetinac <jccetinac@gmail.com>
2021-04-10 07:27:14 -0500

    get state in screen

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
+import { useSelector } from 'react-redux';
 // import { NothingSelected } from './NothingSelected';
 
 
 export const JournalScreen = () => {
+
+    const { indicadoresList} = useSelector( state => state.indicadores);
+    console.log(indicadoresList);
+
     return (
         <div className="journal__main-content">
             
@@ -13,8 +18,14 @@ export const JournalScreen = () => {
 
             <main>
 
-                {/* <NothingSelected /> */}
-                <NoteScreen />
+                {indicadoresList?.map( indicador => (
+                    <div className="jumbotron p-3" key={ indicador.id }>
+                        <h1>{indicador.name}</h1>
+                        <p>{indicador.value}</p>
+                        <p>{indicador.type}</p>
+                        <i>{indicador.autho}</i>
+                    </div>
+                ))}
 
             </main>
 
